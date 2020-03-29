1d9b4dcec047bd98746a7759fdca1ad4d04792db
Bryan Sweeney <bryan.sweeney@uconn.edu>
2020-03-29 12:55:09 -0400

    fixed error for reading phone numbers

--- messaging/text_util.py
+++ messaging/text_util.py
@@ -3,21 +3,21 @@ from config.config_util import config_util
 
 class TextUtil:
     def __init__(self):
-        self.text_creds = config_util.data_map
-        self.client = Client(text_creds['account_sid'], text_creds['auth_token'])
+        self.text_creds = config_util.data_map['text_util']
+        self.client = Client(self.text_creds['account_sid'], self.text_creds['auth_token'])
 
-    def send_text(self, string: stock):
+    def send_text(self, stock):
         text_body = "Check out " + stock
         message = self.client.messages \
             .create(
                  body = text_body,
                  messaging_service_sid = self.text_creds['messaging_service_sid'],
                  to = self.text_creds['text_to_number'],
-                 from_ = self.text_creds['text_to_number']
+                 from_ = self.text_creds['text_from_number']
              )
 
-    def send_text(self, list: stocks):
-        if(len(list) == 0):
+    def send_text_list(self, stocks):
+        if(len(stocks) == 0):
             return
         for stock in stocks:
             self.send_text(stock)
